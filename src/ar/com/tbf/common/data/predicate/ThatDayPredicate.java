cedf0e1cfaa3fbe2d4fbd80e58a79b7a087bc831
Edgardo <Edgardo@DESKTOP-HRVT699>
2019-10-21 19:24:09 -0300

    Se elimina el printstatcktrace para no confundir.

--- src/ar/com/tbf/common/data/predicate/ThatDayPredicate.java
+++ src/ar/com/tbf/common/data/predicate/ThatDayPredicate.java
@@ -43,7 +43,7 @@ public class ThatDayPredicate implements SpecificPredicate{
 			predicate = builder.between( from.<Date>get(key), init.getTime(), end.getTime() );
 
 		}catch( Exception e ) {
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		
 		return predicate;	
