53cf967842eb2a18747c38e36ef208abc11ceae3
Edgardo <egenini@gmail.com>
2021-09-05 21:38:30 -0300

    Se agrega al GenericPredicateBuilder la generación de predicados para los tipos conocidos de Java.

--- src/ar/com/tbf/common/data/GenericPredicateBuilder.java
+++ src/ar/com/tbf/common/data/GenericPredicateBuilder.java
@@ -1,6 +1,10 @@
 package ar.com.tbf.common.data;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
@@ -90,67 +94,147 @@ public class GenericPredicateBuilder {
 				case EQUALITY:
 					if( value.equals( NULL_VALUE )) {
 						
-						predicates.add( builder.isNull(       expression            ) );
+						predicates.add( builder.isNull( expression      ) );
 					}
 					else if( value.equals( FALSE_VALUE )) {
 						
-						predicates.add( builder.equal(       expression, false      ) );
+						predicates.add( builder.equal( expression, false ) );
 					}
 					else if( value.equals( TRUE_VALUE )) {
 						
-						predicates.add( builder.equal(       expression, true       ) );
+						predicates.add( builder.equal( expression, true  ) );
 					}
 					else {
-						predicates.add( builder.equal(       expression, value      ) );
+						predicates.add( builder.equal( expression, value ) );
 					}
 					break;
 				case NEGATION:
 					if( value.equals( NULL_VALUE )) {
 						
-						predicates.add( builder.isNotNull(       expression         ) );
+						predicates.add( builder.isNotNull( expression        ) );
 					}
 					else if( value.equals( FALSE_VALUE )) {
 						
-						predicates.add( builder.notEqual(       expression, false   ) );
+						predicates.add( builder.notEqual(  expression, false ) );
 					}
 					else if( value.equals( TRUE_VALUE )) {
 						
-						predicates.add( builder.notEqual(       expression, true    ) );
+						predicates.add( builder.notEqual(  expression, true  ) );
 					}
 					else {
-						predicates.add( builder.notEqual(       expression, value   ) );
+						predicates.add( builder.notEqual(  expression, value ) );
 					}
 					break;
 				case GREATER_THAN:
-					predicates.add( builder.greaterThan(     from.get(key), value.toString()                   ) );
+					if( value instanceof String ) {
+						predicates.add( builder.greaterThan( from.get(key), value.toString()     ) );
+					}else if( value instanceof Date ) {
+						predicates.add( builder.greaterThan( from.get(key), (Date)value          ) );						
+					}else if( value instanceof Timestamp ) {
+						predicates.add( builder.greaterThan( from.get(key), (Timestamp)value     ) );						
+					}else if( value instanceof Integer ) {
+						predicates.add( builder.greaterThan( from.get(key), (Integer)value       ) );						
+					}else if( value instanceof BigDecimal ) {
+						predicates.add( builder.greaterThan( from.get(key), (BigDecimal )value   ) );						
+					}else if( value instanceof Double ) {
+						predicates.add( builder.greaterThan( from.get(key), (Double)value        ) );						
+					}else if( value instanceof Float ) {
+						predicates.add( builder.greaterThan( from.get(key), (Float)value         ) );						
+					}else if( value instanceof BigInteger ) {
+						predicates.add( builder.greaterThan( from.get(key), (BigInteger)value    ) );						
+					}else if( value instanceof Short ) {
+						predicates.add( builder.greaterThan( from.get(key), (Short)value         ) );						
+					}else if( value instanceof java.sql.Date ) {
+						predicates.add( builder.greaterThan( from.get(key), (java.sql.Date)value ) );						
+					}
 					break;
 				case LESS_THAN:
-					predicates.add( builder.lessThan(        from.get(key), value.toString()                   ) );
+					if( value instanceof String ) {
+						predicates.add( builder.lessThan( from.get(key), value.toString()     ) );
+					}else if( value instanceof Date ) {
+						predicates.add( builder.lessThan( from.get(key), (Date)value          ) );						
+					}else if( value instanceof Timestamp ) {
+						predicates.add( builder.lessThan( from.get(key), (Timestamp)value     ) );						
+					}else if( value instanceof Integer ) {
+						predicates.add( builder.lessThan( from.get(key), (Integer)value       ) );						
+					}else if( value instanceof BigDecimal ) {
+						predicates.add( builder.lessThan( from.get(key), (BigDecimal )value   ) );						
+					}else if( value instanceof Double ) {
+						predicates.add( builder.lessThan( from.get(key), (Double)value        ) );						
+					}else if( value instanceof Float ) {
+						predicates.add( builder.lessThan( from.get(key), (Float)value         ) );						
+					}else if( value instanceof BigInteger ) {
+						predicates.add( builder.lessThan( from.get(key), (BigInteger)value    ) );						
+					}else if( value instanceof Short ) {
+						predicates.add( builder.lessThan( from.get(key), (Short)value         ) );						
+					}else if( value instanceof java.sql.Date ) {
+						predicates.add( builder.lessThan( from.get(key), (java.sql.Date)value ) );						
+					}
 					break;
 				case GREATER_THAN_OR_EQUALS:
-					predicates.add( builder.greaterThanOrEqualTo(     from.get(key), value.toString()                   ) );
+					if( value instanceof String ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), value.toString()     ) );
+					}else if( value instanceof Date ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (Date)value          ) );						
+					}else if( value instanceof Timestamp ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (Timestamp)value     ) );						
+					}else if( value instanceof Integer ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (Integer)value       ) );						
+					}else if( value instanceof BigDecimal ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (BigDecimal )value   ) );						
+					}else if( value instanceof Double ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (Double)value        ) );						
+					}else if( value instanceof Float ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (Float)value         ) );						
+					}else if( value instanceof BigInteger ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (BigInteger)value    ) );						
+					}else if( value instanceof Short ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (Short)value         ) );						
+					}else if( value instanceof java.sql.Date ) {
+						predicates.add( builder.greaterThanOrEqualTo( from.get(key), (java.sql.Date)value ) );						
+					}
 					break;
 				case LESS_THAN_OR_EQUALS:
-					predicates.add( builder.lessThanOrEqualTo(        from.get(key), value.toString()                   ) );
+					if( value instanceof String ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), value.toString()     ) );
+					}else if( value instanceof Date ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (Date)value          ) );						
+					}else if( value instanceof Timestamp ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (Timestamp)value     ) );						
+					}else if( value instanceof Integer ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (Integer)value       ) );						
+					}else if( value instanceof BigDecimal ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (BigDecimal )value   ) );						
+					}else if( value instanceof Double ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (Double)value        ) );						
+					}else if( value instanceof Float ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (Float)value         ) );						
+					}else if( value instanceof BigInteger ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (BigInteger)value    ) );						
+					}else if( value instanceof Short ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (Short)value         ) );						
+					}else if( value instanceof java.sql.Date ) {
+						predicates.add( builder.lessThanOrEqualTo( from.get(key), (java.sql.Date)value ) );						
+					}
 					break;
 				case LIKE:
 					try {
-						predicates.add( builder.like(        (Expression<String>) expression, "%"+ value +"%"  ) );
+						predicates.add( builder.like( (Expression<String>) expression, "%"+ value +"%" ) );
 					}catch(Exception e) {}
 					break;
 				case STARTS_WITH:
 					try {
-						predicates.add( builder.like(        (Expression<String>) expression, value +"%"       ) );
+						predicates.add( builder.like( (Expression<String>) expression, value +"%"      ) );
 					}catch(Exception e) {}
 					break;
 				case ENDS_WITH:
 					try {
-						predicates.add( builder.like(        (Expression<String>) expression, "%"+ value       ) );
+						predicates.add( builder.like( (Expression<String>) expression, "%"+ value      ) );
 					}catch(Exception e) {}
 					break;
 				case CONTAINS:
 					try {
-						predicates.add( builder.like(        (Expression<String>) expression, "%"+ value +"%"  ) );
+						predicates.add( builder.like( (Expression<String>) expression, "%"+ value +"%" ) );
 					}catch(Exception e) {}
 					break;
 				default:
